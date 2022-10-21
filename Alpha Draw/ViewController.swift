09deae27a38fc44b93f87dc464e2aa9d152829c6
AnthonyChen0921 <chenerdong@wustl.edu>
2022-10-21 11:05:49 -0500

    Initial Commit

---
+++ Alpha Draw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alpha Draw
+//
+//  Created by Anthony C on 10/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
