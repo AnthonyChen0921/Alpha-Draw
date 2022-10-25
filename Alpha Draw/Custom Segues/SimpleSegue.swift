5afea89cf40f9d8847b8215dfd7d1a47bedbc57c
AnthonyChen0921 <chenerdong@wustl.edu>
2022-10-25 00:17:18 -0500

    Added Nav Bar to Auth Family

--- Alpha Draw/Custom Segues/SimpleSegue.swift
+++ Alpha Draw/Custom Segues/SimpleSegue.swift
@@ -8,5 +8,21 @@
 import UIKit
 
 class SimpleSegue: UIStoryboardSegue {
-    
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+                       },
+                       completion: { finished in
+                        src.present(dst, animated: false, completion: nil)
+                       }
+        )
+    }
+
 }
