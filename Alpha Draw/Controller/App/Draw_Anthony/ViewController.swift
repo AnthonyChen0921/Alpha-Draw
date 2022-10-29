b98a80838cf320a4cff7b5f7a7c84fa7da86d644
AnthonyChen0921 <chenerdong@wustl.edu>
2022-10-28 23:07:52 -0500

    Text Bubble added

--- Alpha Draw/Controller/App/Draw_Anthony/ViewController.swift
+++ Alpha Draw/Controller/App/Draw_Anthony/ViewController.swift
@@ -11,9 +11,14 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import TextFieldEffects
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
 
+    @IBOutlet weak var wordBubbleCollectionView: UICollectionView!
     @IBOutlet weak var inputPrompt: UITextField!
+
+    var inspirationBubbleString: [String] = ["Apple", "Banana", "Peach", "Orange", "Watermellon", "Kiwi"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +30,41 @@ class ViewController: UIViewController {
 
 
 
+    
 
 
 
 
+    // MARK: - Collection View Data Source
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return inspirationBubbleString.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "wordBubbleCell", for: indexPath) 
+        // create a tinted button in the cell
+        let wordBubbleButton = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+        wordBubbleButton.setTitle(inspirationBubbleString[indexPath.row], for: .normal)
+        wordBubbleButton.backgroundColor = UIColor.purple
+        wordBubbleButton.tintColor = UIColor.lightGray
+        wordBubbleButton.layer.cornerRadius = 10
+        wordBubbleButton.layer.masksToBounds = true
+        wordBubbleButton.addTarget(self, action: #selector(wordBubbleButtonTapped), for: .touchUpInside)
+        cell.addSubview(wordBubbleButton)
+        return cell
+    }
+
+    // MARK: - WordBubble Clicked
+
+    @objc func wordBubbleButtonTapped(sender: UIButton) {
+        print("word bubble button tapped")
+        inputPrompt.text = sender.titleLabel?.text
+    }
+
+
+    // MARK: - Input Prompt
+    
     @IBAction func inputPromptEditingDidBegin(_ sender: Any) {
         changeInputFieldBackgroundToLight(inputField: inputPrompt)
     }
